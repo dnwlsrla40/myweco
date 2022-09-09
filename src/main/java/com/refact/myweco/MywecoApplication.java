09c0ddfd9f6cc129f58a19d16d5f23a2f4266627
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-09-09 20:54:04 +0900

    Initial commit

---
+++ src/main/java/com/refact/myweco/MywecoApplication.java
@@ -0,0 +1,13 @@
+package com.refact.myweco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywecoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywecoApplication.class, args);
+    }
+
+}
